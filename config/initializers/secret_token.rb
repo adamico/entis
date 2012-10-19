285beeb4baff9a178fbe2af7e963976caa66919b
Andrea D'Amico <andrea.damico@gmail.com>
2012-10-19 10:07:36 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Entis::Application.config.secret_token = 'a8a677fd1c7fe2b82bd78d5482954ad8fbcd042bfa6c745994f6202e9115665f2c16445ec4fde9dcccf1afe1368749af54ab151ba9b938d9b10f0f11aabf9157'
