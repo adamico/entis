55dcd0ef746a8fd43629bd9037cf9a5b22d45bda
Andrea D'Amico <andrea.damico@gmail.com>
2012-11-28 17:13:17 +0100

    adding state-center relations and friendly_id

--- vendor/extensions/centers/app/models/refinery/centers/center.rb
+++ vendor/extensions/centers/app/models/refinery/centers/center.rb
@@ -2,12 +2,16 @@ module Refinery
   module Centers
     class Center < Refinery::Core::BaseModel
       self.table_name = 'refinery_centers'
+      extend FriendlyId
 
       attr_accessible :name, :state_id, :service_name, :service_head, :address, :phone, :fax, :email, :website, :creation, :accept_calls_from, :hours, :affiliation, :area_served, :staff, :position
 
       acts_as_indexed :fields => [:name, :service_name, :service_head, :address, :phone, :fax, :email, :website, :creation, :accept_calls_from, :hours, :affiliation, :area_served, :staff]
 
       validates :name, :presence => true, :uniqueness => true
+      belongs_to :state, class_name: "::Refinery::States::State"
+
+      friendly_id :name, use: :slugged
     end
   end
 end
--- vendor/extensions/centers/app/models/refinery/states/state.rb
+++ vendor/extensions/centers/app/models/refinery/states/state.rb
@@ -2,12 +2,16 @@ module Refinery
   module States
     class State < Refinery::Core::BaseModel
       self.table_name = 'refinery_states'
+      extend FriendlyId
 
       attr_accessible :name, :position
 
       acts_as_indexed :fields => [:name]
 
       validates :name, :presence => true, :uniqueness => true
+      has_many :centers, class_name: "::Refinery::Centers::Center"
+
+      friendly_id :name, use: :slugged
     end
   end
 end
