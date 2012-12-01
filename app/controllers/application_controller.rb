c73aa0f982ac4f84ed0f8ece76f47c03a7f1dddf
Andrea D'Amico <andrea.damico@gmail.com>
2012-12-01 14:59:37 +0100

    redirect to private section on login, to root on logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    "/private"
+  end
+
+  def stored_location_for(resource_or_scope)
+    nil
+  end
 end
