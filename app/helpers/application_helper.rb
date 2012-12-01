9b53ff7ec4a4ca8ebc13025e858f5888e9b1999e
Andrea D'Amico <andrea.damico@gmail.com>
2012-12-01 15:15:48 +0100

    set private area link to active if is current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,26 @@ module ApplicationHelper
   def email_def_content(center, email)
     email.gsub(/<\/?p>/, "").split("<br />").map {|item| mail_to(item, nil, replace_at: "_at_", replace_dot: "_dot_")}.join("<br/>").html_safe
   end
+
+  def is_selected?(page)
+    path = request.path
+    path = path.force_encoding('utf-8') if path.respond_to?(:force_encoding)
+
+    # Ensure we match the path without the locale, if present.
+    if ::Refinery.i18n_enabled? and path =~ %r{^/#{::I18n.locale}/}
+      path = path.split(%r{^/#{::I18n.locale}}).last
+      path = "/" if path.blank?
+    end
+
+    # First try to match against a "menu match" value, if available.
+    return true if page.try(:menu_match).present? && path =~ Regexp.new(page.menu_match)
+
+    # Find the first url that is a string.
+    url = [page.url]
+    url << ['', page.url[:path]].compact.flatten.join('/') if page.url.respond_to?(:keys)
+    url = url.last.match(%r{^/#{::I18n.locale.to_s}(/.*)}) ? $1 : url.detect{|u| u.is_a?(String)}
+
+    # Now use all possible vectors to try to find a valid match
+    [path, URI.decode(path)].include?(url) || path == "/#{page.id}"
+  end
 end
