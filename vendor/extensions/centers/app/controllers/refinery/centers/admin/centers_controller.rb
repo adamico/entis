ea7be22d73da37db0b2567ba06ee722277864492
Andrea D'Amico <andrea.damico@gmail.com>
2012-12-27 11:49:16 +0100

    filtering by observer status in backend

--- vendor/extensions/centers/app/controllers/refinery/centers/admin/centers_controller.rb
+++ vendor/extensions/centers/app/controllers/refinery/centers/admin/centers_controller.rb
@@ -4,8 +4,21 @@ module Refinery
       class CentersController < ::Refinery::AdminController
 
         crudify :'refinery/centers/center',
-                :title_attribute => 'name', :xhr_paging => true, :order => "name", :sortable => false
+                :title_attribute => 'name', :xhr_paging => true, :order => "name", :sortable => false,
+                :paging => false
 
+        helper_method :observer_only?
+        before_filter :find_all_observers
+
+        private
+
+        def find_all_observers
+          @observers = Center.with_observer_status.scoped
+        end
+
+        def observer_only?
+          params[:observer] && params[:observer] == 'true' 
+        end
       end
     end
   end
